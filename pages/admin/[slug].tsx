e0eaf27af70d6df89f35ea0de473f904a89384de
unknown <michael.gunhyo@gmail.com>
2023-08-11 09:23:10 -0700

    admin form validation

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from '../../styles/Admin.module.css';
 import AuthCheck from '../../components/AuthCheck';
 import { firestore, auth, serverTime } from '../../lib/firebase';
-import { doc, collection, collectionGroup, updateDoc, } from '@firebase/firestore';
+import { doc, getDoc, collection, collectionGroup, updateDoc, } from '@firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { ErrorMessage } from '@hookform/error-message';
 
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 
-import { useCollectionDataOnce, useDocumentDataOnce } from 'react-firebase-hooks/firestore';
+import { useCollectionDataOnce, useDocumentData } from 'react-firebase-hooks/firestore';
 import { useForm } from 'react-hook-form';
 import ReactMarkdown from 'react-markdown';
 import Link from 'next/link';
@@ -24,14 +26,16 @@ export default function Page() {
 
 function PostManager() {
   const [preview, setPreview] = useState(false);
+  const [user] = useAuthState(auth);
 
   const router = useRouter();
   const { slug } = router.query;
 
-  const currUser = auth.currentUser;
-  const uid = auth.currentUser ? auth.currentUser.uid : null;
-  const postRef = doc(firestore, 'users');
-  const [post] = useDocumentDataOnce(postRef);
+  const uid = user?.uid;
+  const postRef = doc(firestore, 'users', `${uid}`, 'posts', `${slug}`);
+  const [post] = useDocumentData(postRef);
+
+  console.log(postRef);
 
   return (
     <main className={styles.container}>
@@ -58,42 +62,58 @@ function PostManager() {
 }
 
 function PostForm({ defaultValues, postRef, preview }: { defaultValues: any, postRef: any, preview: any }) {
-  const { register, handleSubmit, reset, watch } = useForm({ defaultValues, mode: 'onChange' });
-  const { onChange, onBlur, name, ref } = register('content'); 
+  const { register, handleSubmit, reset, watch, formState, formState: { errors }, setError } = useForm({ defaultValues, mode: 'onChange' });
+  const { isValid, isDirty } = formState;
 
-  const updatePost = async ({content, published} : {content: string, published: boolean}) =>{
+  const updatePost = async ({ content, published }: { content: string, published: boolean }) => {
     await updateDoc(postRef, {
       content,
       published,
       updatedAt: serverTime,
     });
 
-    reset({content, published});
+    reset({ content, published });
 
     toast.success('Post updated successfully!');
   };
 
   return (
     <form onSubmit={handleSubmit(updatePost)}>
-    {preview && (
-      <div className="card">
-        <ReactMarkdown>{watch('content')}</ReactMarkdown>
+      {preview && (
+        <div className="card">
+          <ReactMarkdown>{watch('content')}</ReactMarkdown>
+        </div>
+      )}
+
+      <div className={preview ? styles.hidden : styles.controls}>
+
+        <textarea {...register('content', {
+          required: 'content is required',
+          maxLength: {
+            value: 20000,
+            message: 'content is too long',
+          },
+          minLength: {
+            value: 10,
+            message: 'content is too short'
+          },
+        })}></textarea>
+
+        <ErrorMessage
+          errors={errors}
+          name="content"
+          render={({ message }) => <p className='text-danger'>{message}</p>}
+        />
+
+        <fieldset>
+          <input className={styles.checkbox} type="checkbox" {...register('published')} />
+          <label>Published</label>
+        </fieldset>
+
+        <button type="submit" className="btn-green">
+          Save Changes
+        </button>
       </div>
-    )}
-
-    <div className={preview ? styles.hidden : styles.controls}>
-
-      <textarea name="content" ref={ref}></textarea>
-
-      <fieldset>
-        <input className={styles.checkbox} name="published" type="checkbox" ref={ref} />
-        <label>Published</label>
-      </fieldset>
-
-      <button type="submit" className="btn-green">
-        Save Changes
-      </button>
-    </div>
-  </form>
+    </form>
   );
 }
